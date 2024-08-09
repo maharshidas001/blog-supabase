cdcd00264d865f183c96151f649fd386efdf2772
Maharshi Das <maharshidas001@gmail.com>
2024-08-09 11:04:47 +0530

    React Router Dom Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Header } from '@/components';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '@/pages';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
