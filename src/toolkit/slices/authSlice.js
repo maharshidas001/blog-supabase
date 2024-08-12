c5d8998bb017046fae6e294ec3636f7df8e2ac19
Maharshi Das <maharshidas001@gmail.com>
2024-08-12 10:00:43 +0530

    Auth slice

---
+++ src/toolkit/slices/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isSignedIn: false,
+  user: null
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isSignedIn = action.payload.isSignedIn;
+      state.user = action.payload.user;
+    },
+    logout: (state) => {
+      state.isSignedIn = false;
+      state.user = null;
+    }
+  }
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
